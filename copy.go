f28da5e274d96961e0fe95a5be210388ff4c30c8
mathieu <hilaire.mathieu@gmail.com>
2016-02-12 18:44:45 +0100

    Implemented ShadowCopy function.

--- copy.go
+++ copy.go
@@ -69,6 +69,84 @@ func Copy(source, destination string) (err error) {
 	})
 }
 
+func ShadowCopy(source, destination string) (err error) {
+	srcInfo, srcErr := os.Stat(source)
+	if srcErr != nil {
+		return srcErr
+	}
+	dstInfo, dstErr := os.Stat(destination)
+	if srcInfo.IsDir() && dstInfo != nil && !dstInfo.IsDir() && !os.IsNotExist(dstErr) {
+		return fmt.Errorf("Cannot overwrite non-directory '%s' with directory '%s'", destination, source)
+	}
+	dstIsDir := false
+	if dstInfo != nil {
+		dstIsDir = dstInfo.IsDir()
+	}
+	return filepath.Walk(source, func(pathFile string, info os.FileInfo, e error) error {
+		if !info.IsDir() && info.Mode().IsRegular() {
+			// Copy file
+			cpyPath := destination
+			if dstInfo != nil && dstInfo.IsDir() {
+				if srcInfo.IsDir() {
+					if dstIsDir {
+						cpyPath = pathFile[len(path.Dir(source)):]
+					} else {
+						cpyPath = pathFile[len(source):]
+					}
+				} else {
+					cpyPath = path.Base(pathFile)
+				}
+				cpyPath = path.Join(destination, cpyPath)
+			}
+			e = shadowCopyFile(pathFile, cpyPath)
+			if e != nil {
+				err = e
+				return err
+			}
+		} else if info.IsDir() {
+			// Copy directory
+			dirpath := destination
+			if dstInfo != nil && dstInfo.IsDir() {
+				if dstIsDir {
+					dirpath = path.Join(destination, pathFile[len(path.Dir(source)):])
+				} else {
+					dirpath = path.Join(destination, pathFile[len(source):])
+				}
+			}
+			e = os.Mkdir(dirpath, 0755)
+			if e != nil {
+				err = e
+				return err
+			}
+			dstInfo, e = os.Stat(dirpath)
+			if e != nil {
+				err = e
+				return err
+			}
+		}
+		return nil
+	})
+}
+
+func shadowCopyFile(source, destination string) (err error) {
+	in, err := os.Open(source)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+	out, err := os.Create(destination)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		cerr := out.Close()
+		if err == nil {
+			err = cerr
+		}
+	}()
+	return err
+}
+
 func copyFile(source, destination string) (err error) {
 	in, err := os.Open(source)
 	if err != nil {
--- copy_test.go
+++ copy_test.go
@@ -264,6 +264,176 @@ func TestCopyDirectoryToExistingDirectory(t *testing.T) {
 		return nil
 	})
 }
+
+func TestShadowCopyFileToFile(t *testing.T) {
+	file1, err := testCreateRandomFile("")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(file1)
+	file2 := path.Join(os.TempDir(), "TestShadowCopyFileToFile"+strconv.Itoa(rand.Int()))
+	defer os.Remove(file2)
+	err = ShadowCopy(file1, file2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !IsFileEmpty(file2) {
+		t.Fatalf("`%s` should be empty", file2)
+	}
+}
+
+func TestShadowCopyInexistingFile(t *testing.T) {
+	file1 := path.Join(os.TempDir(), "TestShadowCopyInexistingFile"+strconv.Itoa(rand.Int()))
+	file2 := path.Join(os.TempDir(), "TestShadowCopyInexistingFile"+strconv.Itoa(rand.Int()))
+	err := ShadowCopy(file1, file2)
+	if err == nil {
+		t.Fatal("You shouldn't be able to copy a file that doesn't exist !")
+	}
+}
+
+func TestShadowCopyFileToDirectory(t *testing.T) {
+	file, err := testCreateRandomFile("")
+	defer os.Remove(file)
+	dir, err := ioutil.TempDir(os.TempDir(), "TestShadowCopyFileToDirectory")
+	defer os.RemoveAll(dir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = ShadowCopy(file, dir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	_, err = os.Stat(path.Join(dir, path.Base(file)))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !IsFileEmpty(path.Join(dir, path.Base(file))) {
+		t.Fatalf("`%s` should be empty", path.Join(dir, path.Base(file)))
+	}
+}
+
+func TestShadowCopyDirectoryToFile(t *testing.T) {
+	file, err := testCreateRandomFile("")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.Remove(file)
+	dir, err := ioutil.TempDir(os.TempDir(), "TestShadowCopyDirectoryToFile")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+	err = ShadowCopy(dir, file)
+	if err == nil {
+		t.Fatal("You shouldn't be able to copy a directory into a file !")
+	}
+}
+
+func TestShadowCopyDirectoryToNonExistingDirectory(t *testing.T) {
+	maindir, err := ioutil.TempDir(os.TempDir(), "source")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(maindir)
+	dir, err := ioutil.TempDir(maindir, "dir")
+	if err != nil {
+		t.Fatal(err)
+	}
+	subdir, err := ioutil.TempDir(dir, "subdir")
+	if err != nil {
+		t.Fatal(err)
+	}
+	files := make([]string, 25)
+	for i := 0; i < len(files); i++ {
+		n := rand.Int()
+		if n%3 == 0 {
+			files[i], err = testCreateRandomFile(maindir)
+		} else if n%3 == 1 {
+			files[i], err = testCreateRandomFile(dir)
+		} else {
+			files[i], err = testCreateRandomFile(subdir)
+		}
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+	cpyDir := path.Join(os.TempDir(), "destination"+strconv.Itoa(rand.Int()))
+	err = ShadowCopy(maindir, cpyDir)
+	defer os.RemoveAll(cpyDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	filepath.Walk(maindir, func(pathFile string, info os.FileInfo, err error) error {
+		cpyPath := pathFile[len(maindir):]
+		cpyPath = path.Join(cpyDir, cpyPath)
+		// Check if file or directory has been created
+		_, err = os.Stat(cpyPath)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !info.IsDir() {
+			if !IsFileEmpty(cpyPath) {
+				t.Fatalf("`%s` should be empty", cpyPath)
+			}
+		}
+		return nil
+	})
+}
+
+func TestShadowCopyDirectoryToExistingDirectory(t *testing.T) {
+	maindir, err := ioutil.TempDir(os.TempDir(), "source")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(maindir)
+	dir, err := ioutil.TempDir(maindir, "dir")
+	if err != nil {
+		t.Fatal(err)
+	}
+	subdir, err := ioutil.TempDir(dir, "subdir")
+	if err != nil {
+		t.Fatal(err)
+	}
+	files := make([]string, 25)
+	for i := 0; i < len(files); i++ {
+		n := rand.Int()
+		if n%3 == 0 {
+			files[i], err = testCreateRandomFile(maindir)
+		} else if n%3 == 1 {
+			files[i], err = testCreateRandomFile(dir)
+		} else {
+			files[i], err = testCreateRandomFile(subdir)
+		}
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+	cpyDir, err := ioutil.TempDir(os.TempDir(), "destination")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(cpyDir)
+	err = ShadowCopy(maindir, cpyDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	filepath.Walk(maindir, func(pathFile string, info os.FileInfo, err error) error {
+		cpyPath := pathFile[len(path.Dir(maindir)):]
+		cpyPath = path.Join(cpyDir, cpyPath)
+		// Check if file or directory has been created
+		_, err = os.Stat(cpyPath)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !info.IsDir() {
+			if !IsFileEmpty(cpyPath) {
+				t.Fatalf("`%s` should be empty", cpyPath)
+			}
+		}
+		return nil
+	})
+}
+
 func TestIsFileEmptyWithEmptyFile(t *testing.T) {
 	file, err := ioutil.TempFile(os.TempDir(), "TestIsFileEmptyWithEmptyFile")
 	if err != nil {
